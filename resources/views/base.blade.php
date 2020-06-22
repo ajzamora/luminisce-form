3ae2dddb0e3c943fe8333ada00884f42e1c83a47
zam <ajmzamora@gmail.com>
2020-06-22 18:43:47 -0400

    change page title

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel 7</title>
+    <title>Luminisce Forms</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
